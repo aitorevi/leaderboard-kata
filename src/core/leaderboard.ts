b3d5425c50e4534204eda69089dcb7d6aabd1108
Aitor Reviriego Amor <aitorevi@gmail.com>
2023-11-14 23:01:36 +0100

    :recycle: rename constants

--- src/core/leaderboard.ts
+++ src/core/leaderboard.ts
@@ -1,14 +1,14 @@
-import { PlayerWithPosition } from "interfaces/playerWithPosition";
+import {PlayerWithPosition} from "interfaces/playerWithPosition";
 import {Player} from "../interfaces/player";
 
-const byScore = (players: Player[]) => players.slice().sort((a, b) => b.score - a.score);
-const byIndex = (players: Player[]): PlayerWithPosition[] => {
+const sortByScore = (players: Player[]) => players.slice().sort((a, b) => b.score - a.score);
+const addsPositions = (players: Player[]): PlayerWithPosition[] => {
     return players.map((player, index) => {
-      const position = index + 1;
-      return { ...player, position };
+        const position = index + 1;
+        return {...player, position};
     });
-  };
-    const tied = (players: PlayerWithPosition[]) => {
+};
+const groupsTiedPositions = (players: PlayerWithPosition[]) => {
     return players.map((player, index) => {
         const previousPlayer = players[index - 1];
         const playersTied = index > 0 && player.score == previousPlayer.score;
@@ -21,11 +21,11 @@ const byIndex = (players: Player[]): PlayerWithPosition[] => {
         return player
     })
 };
-const byName = (players: Player[]) => players.slice().sort((a, b) => a.name.localeCompare(b.name));
+const sortByName = (players: Player[]) => players.slice().sort((a, b) => a.name.localeCompare(b.name));
 
 export function leaderboard(players: Player[]) {
-    const sortedPlayers = byScore(players),
-        addPosition = byIndex(sortedPlayers),
-        groupedPlayers = tied(addPosition);
-    return byName(groupedPlayers);
+    const sortedPlayers = sortByScore(players),
+        addedPositionPlayers = addsPositions(sortedPlayers),
+        groupedPlayers = groupsTiedPositions(addedPositionPlayers);
+    return sortByName(groupedPlayers);
 }
